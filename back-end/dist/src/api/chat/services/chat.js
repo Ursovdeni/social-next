90f1dea8dfb4535693929d013d9115d14aca5ed9
Ursovdeni <142998698+Ursovdeni@users.noreply.github.com>
2023-12-25 15:27:06 +0300

    Add files via upload

---
+++ back-end/dist/src/api/chat/services/chat.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * chat service
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreService('api::chat.chat');
